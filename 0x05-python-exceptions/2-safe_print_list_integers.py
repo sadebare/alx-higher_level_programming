40f6465d0f2db27896fc28e943f0be8eb8642485
sadebare <adebaresulaimon04@gmail.com>
2022-06-20 12:20:41 +0100

    exception mandatory task

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for element in range(x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            continue
+    print("")
+    return (count)
