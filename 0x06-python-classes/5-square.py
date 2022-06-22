638593801a68f4551199fb68207fc44d8fb8b58f
sadebare <adebaresulaimon04@gmail.com>
2022-06-22 03:05:38 +0100

    printing a square using a square method

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" module that creates a class of Square """
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """ Square class with an attribute of size """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """ method that retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ method that set the value of size """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ method that returns the current square area """
+        return self.__size**2
+
+    def my_print(self):
+        """ method that prints in stdout the square with the character # """
+        if self.__size == 0:
+            print('')
+        for n in range(self.__size):
+            for o in range(self.__size):
+                print("#", end="")
+            print()
