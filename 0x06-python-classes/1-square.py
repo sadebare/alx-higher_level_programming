b0bc2ef1cb407184714a533d986dd2fd488dca7a
sadebare <adebaresulaimon04@gmail.com>
2022-06-22 01:39:29 +0100

    class square that has the attribute of size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this module defines a Class Square with an attribute of size """
+
+
+class Square:
+    """Square class with an attribute of size"""
+
+    def __init__(self, size):
+        """initializing self and class"""
+        self.__size = size
