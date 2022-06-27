9bf4db7c67803b87d332d06d2feae033669f7ece
sadebare <adebaresulaimon04@gmail.com>
2022-06-27 17:02:09 +0100

    class of Rectangle that set and retrieve the width and height of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,10 +20,6 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Method that set the width to value
-        Args:
-            value - width to be set to.
-        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -37,10 +33,6 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ Method to set the height of the rectangle to value
-        Args:
-            value - height to set the height to
-        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
