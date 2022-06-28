595456db81256ea676ddb0e253abd90a0d1588d7
sadebare <adebaresulaimon04@gmail.com>
2022-06-28 13:19:11 +0100

    Everything is object mandatory tasks

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module that contains the copy_list function """
+
+
+def copy_list(l):
+    """ Function that returns a copy of list
+    Args:
+        l - list
+    Returns:
+        A new copy of list l
+    """
+    temp = list(l)
+    return temp
