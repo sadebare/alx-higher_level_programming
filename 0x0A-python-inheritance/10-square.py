e5e9af70ba9b7792e2d54d6499c55756a8e0ec9a
sadebare <adebaresulaimon04@gmail.com>
2022-07-04 17:51:19 +0100

    Square #2

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -47,16 +47,9 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """Class Square inherits from Rectangle"""
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
+        """initializer"""
         super().__init__(size, size)
-        self.integer_validator("size", size)
+        self.integer_validator('size', size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -47,19 +47,17 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """Class Square inherits from Rectangle"""
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
+        """initializer"""
         super().__init__(size, size)
-        self.integer_validator("size", size)
+        self.integer_validator('size', size)
         self.__size = size
 
     def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        """area validation for square"""
+        return self.__size * self.__size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """return a str representaion of square"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
