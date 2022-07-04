665688e57641f4c02bce724cc8dfcde8c7db3f43
sadebare <adebaresulaimon04@gmail.com>
2022-07-04 16:39:16 +0100

    improves Geometry with the method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module with the class BaseGeometry """
+
+
+class BaseGeometry:
+    """
+    Class that contains area
+    """
+
+    @classmethod
+    def area(self):
+        """ method that raises an exception """
+
+        raise Exception("area() is not implemented")
