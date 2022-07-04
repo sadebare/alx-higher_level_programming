7de996640704395b761b4faae44bb476da08a3bc
sadebare <adebaresulaimon04@gmail.com>
2022-07-04 17:18:21 +0100

    Full rectangle impplementation with area and __str__

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" module with the class BaseGeometry """
+
+
+class BaseGeometry:
+    """
+    Class that contains area
+    """
+
+    @classmethod
+    def area(self):
+        """ method that raises an exception """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ methods that validates both value """
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialization of privates"""
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def area(self):
+        """ method that prints the area of a rectangle """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """ method that returns the implementation of the class in string format """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
