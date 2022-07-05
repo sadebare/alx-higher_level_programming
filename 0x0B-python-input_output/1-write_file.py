898aa89e5bdde77e886525adb5d9e72de1cc3c71
sadebare <adebaresulaimon04@gmail.com>
2022-07-05 13:18:41 +0100

    write to a file function

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module that contains write_file function """
+
+
+from asyncore import write
+
+
+def write_file(filename="", text=""):
+    """
+    function that writes a string to a text file (UTF8)
+    and returns the number of characters written:
+    """
+    with open(filename, "w+", encoding="utf-8") as a_file:
+        return a_file.write(text)
