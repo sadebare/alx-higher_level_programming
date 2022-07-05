cfcab79137a7cad27c5037682904c7067eca4a6a
sadebare <adebaresulaimon04@gmail.com>
2022-07-05 15:01:48 +0100

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" module that contains the class Student """
+
+
+class Student:
+    """
+    A class of student and method to_json
+    Attributes:
+        first_name (str): name of student.
+        last_name (str): name of student.
+        age (int): age of student.
+    Methods:
+        __init__ - initializes the Student instance.
+        to_json - retrieves dictionary repr of Student instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """ initializing first_name, last_name and age """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+            Args:
+            attr (list): attribute names that are to be retrieved.
+        """
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
