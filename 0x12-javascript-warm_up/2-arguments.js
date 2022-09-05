756cd3c846325b73ac0199ceed3d1a4d55b34aaa
sadebare <adebaresulaimon04@gmail.com>
2022-09-05 23:07:44 +0100

    js:: passing arguments from the command line

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Script that prints a new message
+depending on the number of argument passed
+ */
+const noArgs = process.argv.length;
+if (noArgs < 3){
+    console.log('No argument');
+} else if (noArgs === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
