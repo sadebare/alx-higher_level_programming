9f34b6a17af875658e0fcb6fa52923f921f1ae19
sadebare <adebaresulaimon04@gmail.com>
2022-09-06 00:22:44 +0100

    javascript:: determine an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 /* Write a script that prints My number: <first argument converted in integer>
-  if the first argument can be converted to an integer
-*/
-const x = parseInt(process.argv[2]);
-if (Number.isNaN(x)) {
+  if the first argument can be converted to an integer */
+const argv = parseInt(process.argv[2]);
+if (Number.isNaN(argv)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', x);
+  console.log('My number: ', argv);
 }
