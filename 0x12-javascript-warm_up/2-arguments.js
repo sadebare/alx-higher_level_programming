6502a741f5917a5d2602757e56871b4a58b14594
sadebare <adebaresulaimon04@gmail.com>
2022-09-05 23:35:13 +0100

    js:: arguments in js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,10 @@ Script that prints a new message
 depending on the number of argument passed
  */
 const noArgs = process.argv.length;
-if (noArgs < 3){
-    console.log('No argument');
+if (noArgs < 3) {
+  console.log('No argument');
 } else if (noArgs === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
