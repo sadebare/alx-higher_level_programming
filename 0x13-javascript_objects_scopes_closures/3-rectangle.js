7329a5fa3f7c5605521374a305337d1419bc778b
sadebare <adebaresulaimon04@gmail.com>
2022-09-06 14:13:38 +0100

    javascript:: #3 rectangle 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// a class Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
@@ -7,6 +6,12 @@ class Rectangle {
     } else {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
