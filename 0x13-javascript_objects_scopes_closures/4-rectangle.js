986bed6e38805480ff5ff3e2e5639ccfd30b6fbe
sadebare <adebaresulaimon04@gmail.com>
2022-09-06 14:22:30 +0100

    javascript:: rotate and double method.

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let holder = this.width
+    this.width = this.height;
+    this.height = holder;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
