18ead8de2d052b4c618965b481f2b00bbfe89f86
sadebare <adebaresulaimon04@gmail.com>
2022-09-09 20:53:53 +0100

    started

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,14 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3360, user=argv[1], passwd=argv[2], db=argv[3]);
+if __name__ = "__main__":
+  db = MySQLdb.connect(host="localhost", port=3360, user=argv[1],
+                       passwd=argv[2], db=argv[3]);
 
-curl = db.cursor();
-curl.execute("SELECT * FROM hbtn_0e_0_usa ORDER BY id ASC")
-rows = curl.fetchall()
-for row in rows:
-    print(row)
-curl.close()
-db.close()
+  curl = db.cursor();
+  curl.execute("SELECT * FROM hbtn_0e_0_usa ORDER BY id ASC")
+  rows = curl.fetchall()
+  for row in rows:
+      print(row)
+  curl.close()
+  db.close()
