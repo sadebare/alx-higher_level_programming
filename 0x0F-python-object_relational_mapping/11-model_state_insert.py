60cd2fab89de75c5e20fdadf3b8060992ddfbacf
sadebare <adebaresulaimon04@gmail.com>
2022-09-10 06:52:23 +0100

    SQLAlchemy:: Add a new state to the State table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+
+from ast import arg
+from unicodedata import name
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print('{}'.format(new_state.id))
+    session.close()
