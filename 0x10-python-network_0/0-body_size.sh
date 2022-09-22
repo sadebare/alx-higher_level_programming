469a02ed28f646d7dd3a0880fe10e7b4c4ce2f9a
sadebare <adebaresulaimon04@gmail.com>
2022-09-22 13:35:32 +0100

    display content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL, sends a request to that URL
-#and displays the size of the body of the response
+# takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl -sI GET "$1" |  grep -i "Content-Length" | cut -d " " -f2
