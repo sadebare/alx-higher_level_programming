ac6b98560258bde499eb7552fe6ac33f8927f4ac
sadebare <adebaresulaimon04@gmail.com>
2022-09-23 18:46:53 +0100

    request header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL,
+sends a request to the URL and displays the value of
+the variable X-Request-Id in the response header"""
+import requests
+import sys
+
+
+def header():
+    """header"""
+    result = requests.get(sys.argv[1])
+    print(result.headers.get("X-Request-Id", None))
+
+if __name__ == "__main__":
+    header()
